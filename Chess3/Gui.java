14e0d347a677dc0de0478b1dbc8df6b5765b9f0f
Devendra <devendrashivraj@gmail.com>
2014-06-15 17:46:40 -0400

    update

--- Chess3/Gui.java
+++ Chess3/Gui.java
@@ -27,13 +27,20 @@ public class Gui extends JFrame implements ActionListener{
     Scanner s;
     boolean turn=true;
     boolean auto=false;
+    boolean singlePlayMode = false;
     Coordinate start,end;
- 
-
- 	Piece p1;
- 	Piece p2;
+    Piece p1;
+    Piece p2;
 
     public Gui(String[]args) {
+	Object[] possibilities = {"Two-Player Mode", "Single-Player Mode"};
+	String k = (String)JOptionPane.showInputDialog(frame, "Choose Your Preferred Mode of Game:","Game Mode",JOptionPane.PLAIN_MESSAGE,null, possibilities,"Two-Player Mode");
+	if (k == null){
+	    System.exit(0);
+	}
+	if (k.equals("Single-Player Mode")){
+	    singlePlayMode = true;
+	}
 	board =new GameBoard();
 	board.initialize();
         initComponents();
@@ -51,98 +58,97 @@ public class Gui extends JFrame implements ActionListener{
     public GameBoard getBoard(){
 	return board;
     }
-
     
     private void initComponents() {
-		GridLayout grid1=new GridLayout(1, 8);
-		GridLayout grid2=new GridLayout(8, 1);
-		JPanel panel1=new JPanel();
-		JPanel panel2=new JPanel();
-		JPanel panel3=new JPanel();
-		JPanel panel4=new JPanel();
-		panel1.setLayout(grid1);
-		panel2.setLayout(grid2);
-		panel3.setLayout(grid1);
-		panel4.setLayout(grid2);
-		panel1.add(new JPanel());
-		panel3.add(new JPanel());	
-		chessboardmain = new JPanel();
-		for(int y = 0; y< 8; y++){             
-		    for(int x = 0;x < 8; x++){
-			ImageIcon icon=board.getBoard()[x][7-y].getAvatar();
-			board.pattern[x][7-y].setIcon(icon);
-			board.pattern[x][7-y].setPreferredSize(new Dimension(75, 75));
-			board.pattern[x][7-y].addActionListener(this);
-			board.pattern[x][7-y].setBackground(now);
-			if(now.equals(white)){
-			    now=black;
-			}else{
-			    now=white;
-			}
-			chessboardmain.add(board.pattern[x][7-y]);		
-		    }
-		    if(now.equals(white)){
-			now=black;
-		    }else{
-			now=white;
-		    }
-		    panel1.add(new JLabel("  "+letters.substring(y,y+1)+"  "));
-		    panel2.add(new JLabel(Integer.toString(8-y)));
-		    panel3.add(new JLabel("  "+letters.substring(y,y+1)+"  "));
-		    panel4.add(new JLabel(Integer.toString(8-y)));
+	GridLayout grid1=new GridLayout(1, 8);
+	GridLayout grid2=new GridLayout(8, 1);
+	JPanel panel1=new JPanel();
+	JPanel panel2=new JPanel();
+	JPanel panel3=new JPanel();
+	JPanel panel4=new JPanel();
+	panel1.setLayout(grid1);
+	panel2.setLayout(grid2);
+	panel3.setLayout(grid1);
+	panel4.setLayout(grid2);
+	panel1.add(new JPanel());
+	panel3.add(new JPanel());	
+	chessboardmain = new JPanel();
+	for(int y = 0; y< 8; y++){             
+	    for(int x = 0;x < 8; x++){
+		ImageIcon icon=board.getBoard()[x][7-y].getAvatar();
+		board.pattern[x][7-y].setIcon(icon);
+		board.pattern[x][7-y].setPreferredSize(new Dimension(75, 75));
+		board.pattern[x][7-y].addActionListener(this);
+		board.pattern[x][7-y].setBackground(now);
+		if(now.equals(white)){
+		    now=black;
+		}else{
+		    now=white;
 		}
-		frame.setTitle("Chess");
-	    	frame.getContentPane().add(BorderLayout.CENTER, chessboardmain);
-		frame.getContentPane().add(BorderLayout.NORTH, panel1);
-		frame.getContentPane().add(BorderLayout.WEST, panel2);
-		frame.getContentPane().add(BorderLayout.SOUTH, panel3);
-		frame.getContentPane().add(BorderLayout.EAST, panel4);
-		frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-		frame.setSize(675, 700);
-		frame.setLocation(100, 0);
-		frame.setVisible(true);
-		frame.setResizable(false);
+		chessboardmain.add(board.pattern[x][7-y]);		
+	    }
+	    if(now.equals(white)){
+		now=black;
+	    }else{
+		now=white;
+	    }
+	    panel1.add(new JLabel("  "+letters.substring(y,y+1)+"  "));
+	    panel2.add(new JLabel(Integer.toString(8-y)));
+	    panel3.add(new JLabel("  "+letters.substring(y,y+1)+"  "));
+	    panel4.add(new JLabel(Integer.toString(8-y)));
+	}
+	frame.setTitle("Chess");
+	frame.getContentPane().add(BorderLayout.CENTER, chessboardmain);
+	frame.getContentPane().add(BorderLayout.NORTH, panel1);
+	frame.getContentPane().add(BorderLayout.WEST, panel2);
+	frame.getContentPane().add(BorderLayout.SOUTH, panel3);
+	frame.getContentPane().add(BorderLayout.EAST, panel4);
+	frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+	frame.setSize(675, 700);
+	frame.setLocation(100, 0);
+	frame.setVisible(true);
+	frame.setResizable(false);
     }  
-
+    
     public void actionPerformed(ActionEvent event){
-		if(board.getdone()){
-		    return;
-		}
-		for(int x = 0; x< 8; x++){             
-		    for(int y = 0;y < 8; y++){
-			if (event.getSource()==board.pattern[x][y]){
-			    if(start == null ){
-				if(!(board.getPiece(x,y) instanceof NullPiece)){   
-				    now=board.pattern[x][y].getBackground();
-				    board.pattern[x][y].setBackground(Color.GREEN);
-				    start=new Coordinate(x,y);
-				}
-			    }
-			    else if(!(board.getPiece(x,y) instanceof NullPiece) && board.getPiece(x, y).isWhite() && turn){ 
-			    	clearBackground();  
-				    now=board.pattern[x][y].getBackground();
-				    board.pattern[x][y].setBackground(Color.GREEN);
-				    start=new Coordinate(x,y);
-				}
-				else if(!(board.getPiece(x,y) instanceof NullPiece) && !(board.getPiece(x, y).isWhite()) && !turn){ 
-			    	clearBackground();  
-				    now=board.pattern[x][y].getBackground();
-				    board.pattern[x][y].setBackground(Color.GREEN);
-				    start=new Coordinate(x,y);
-				}
-			   	else{
-				end=new Coordinate(x,y);
-				if(!turn(new Move(start,end))){
-				    start=null;
-				    end=null;
-				}else if(board.getdone()){
-				    JOptionPane.showMessageDialog(new JFrame(),board.win());
-				}
-				clearBackground();
-			    }
+	if(board.getdone()){
+	    return;
+	}
+	for(int x = 0; x< 8; x++){             
+	    for(int y = 0;y < 8; y++){
+		if (event.getSource()==board.pattern[x][y]){
+		    if(start == null ){
+			if(!(board.getPiece(x,y) instanceof NullPiece)){   
+			    now=board.pattern[x][y].getBackground();
+			    board.pattern[x][y].setBackground(Color.GREEN);
+			    start=new Coordinate(x,y);
 			}
 		    }
+		    else if(!(board.getPiece(x,y) instanceof NullPiece) && board.getPiece(x, y).isWhite() && turn){ 
+			clearBackground();  
+			now=board.pattern[x][y].getBackground();
+			board.pattern[x][y].setBackground(Color.GREEN);
+			start=new Coordinate(x,y);
+		    }
+		    else if(!(board.getPiece(x,y) instanceof NullPiece) && !(board.getPiece(x, y).isWhite()) && !turn){ 
+			clearBackground();  
+			now=board.pattern[x][y].getBackground();
+			board.pattern[x][y].setBackground(Color.GREEN);
+			start=new Coordinate(x,y);
+		    }
+		    else{
+			end=new Coordinate(x,y);
+			if(!turn(new Move(start,end))){
+			    start=null;
+			    end=null;
+			}else if(board.getdone()){
+			    JOptionPane.showMessageDialog(new JFrame(),board.win());
+			}
+			clearBackground();
+		    }
 		}
+	    }
+	}
     }
 
     public void clearBackground(){
